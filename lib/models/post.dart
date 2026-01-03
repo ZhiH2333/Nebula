498973eb67e16e79bed47057b02dfe0492a803b9
ZhiH2333 <166639072@qq.com>
2026-01-03 10:59:01 +0800

    v0.4-firestore-read

--- lib/models/post.dart
+++ lib/models/post.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Post {
   final String id;
   final String title;
@@ -12,4 +14,15 @@ class Post {
     required this.author,
     required this.createdAt,
   });
+
+  factory Post.fromFirestore(DocumentSnapshot doc) {
+    final data = doc.data() as Map<String, dynamic>;
+    return Post(
+      id: doc.id,
+      title: data['title'] as String? ?? '',
+      body: data['body'] as String? ?? '',
+      author: data['author'] as String? ?? '',
+      createdAt: (data['createdAt'] as Timestamp).toDate(),
+    );
+  }
 }
--- lib/providers/posts_provider.dart
+++ lib/providers/posts_provider.dart
@@ -1,8 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import '../models/post.dart';
-import '../data/mock_posts.dart';
 
 final postsProvider = FutureProvider<List<Post>>((ref) async {
-  await Future.delayed(const Duration(seconds: 2));
-  return mockPosts;
+  final snapshot = await FirebaseFirestore.instance.collection('posts').get();
+  return snapshot.docs.map((doc) => Post.fromFirestore(doc)).toList();
 });
